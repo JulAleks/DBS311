6331024ef0c5b4823f15c161d8ce818c90f87d81
Audrey (^０^)ノ <109978653+audreylearns@users.noreply.github.com>
2023-10-12 23:11:11 -0400

    clean up +  added comments

--- DBS.sql
+++ DBS.sql
@@ -3,15 +3,13 @@ Assignment 1 - DBS 311
 Julia Alekssev, 051292134
 Ka Ying Chan, 123231227
 Audrey Duzon, 019153147
-2023-10-06 <<================== UPDATE DATE!
+2023-10-23
 **********************************/
 
 -- Q1
 SELECT 
     employee_id,
-  --  SUBSTR( UPPER(SUBSTR(last_name,0,1)) || SUBSTR(last_name, 2) || ', ' || UPPER(SUBSTR(first_name,0,1))|| SUBSTR(first_name, 2), 0,25) AS full_name, 
-  --  SUBSTR(last_name || ', ' || first_name, 1, 25) AS Fullname, -- Nicole: I think we might not need rpad(25) as padding 25 characters is not required, we only need to limit the word count of fullname
-      SUBSTR(INITCAP(last_name) || ', ' || INITCAP(first_name), 1, 25) AS Fullname,   --Julia: I think this should be ok, i added something becasue the lower case if bruce pissed my OCD off
+    SUBSTR(INITCAP(last_name) || ', ' || INITCAP(first_name), 1, 25) AS Fullname,  
     job_id,
     TO_CHAR(LAST_DAY(hire_date), '[Month ddth "of" YYYY]') AS "Start Date"
 FROM employees
@@ -23,7 +21,7 @@ ORDER BY
 
 ---------------------------------------------------------------------------------------------------
 --Q2
-
+--as per clint, 1 use of CASE is ok. This is the only question using case
 SELECT 
     'Emp# ' || employee_id || ' named ' || first_name || ' ' || last_name || ' who is ' || job_id || ' will have a new salary of $' || 
     CASE 
@@ -37,52 +35,7 @@ WHERE
     UPPER(job_id) NOT LIKE '%PRES%'
 ORDER BY salary DESC;
 
-/*
-SELECT
-    'Employees with increased Pay' AS "heading",
-    'Emp# ' || d.employee_id || ' named ' || d.first_name || ' ' || d.last_name || ' who is ' || d.job_id || ' will have a new salary of $' || 
-    (CASE WHEN d.job_id LIKE '%VP' THEN (d.salary * 1.25) ELSE (d.salary * 1.18) END) AS "sample line"
-FROM (
-    SELECT DISTINCT
-        m.employee_id,
-        m.first_name,
-        m.last_name,
-        m.job_id,
-        m.salary,
-        TO_CHAR(m.salary, '$999,999.99') AS total_annual_pay
-    FROM employees e
-    JOIN employees m ON e.manager_id = m.employee_id
-    WHERE m.job_id NOT LIKE '%PRES' 
-        AND (m.salary < 6500 OR m.salary > 11500)
-) d
---ORDER BY d.employee_id;
---sort by top salaries first
-ORDER BY 
-    salary DESC;
-*/
 
--- Q2 Original Modified:                WE CAN'T USE ID CASE IF IN THIS ASS 
---SELECT
---    'Emp# ' || employee_id || ' named ' || first_name || ' ' || last_name || ' who is ' || job_id || ' will have a new salary of $' || 
---    (CASE 
---        WHEN UPPER(job_id) LIKE '%VP' THEN (salary * 1.25) -- TO UPPER     //CLINT SAID WE CANNOT USE CASE 
---        ELSE (salary * 1.18) 
---    END) AS "Employees with increased Pay"
---FROM (
---    SELECT DISTINCT
---        m.employee_id,
---        m.first_name,
---        m.last_name,
---        m.job_id,
---        m.salary
---        -- TO_CHAR(m.salary, '$999,999.99') AS total_annual_pay -- NOT USED
---    FROM employees e
---        JOIN employees m ON e.manager_id = m.employee_id
---    WHERE m.job_id NOT LIKE '%PRES' 
---        AND (m.salary <= 6500 OR m.salary >= 11500) -- SHOULD BE >= AND <=
---    ) -- Alias 'd' not needed
---ORDER BY 
---    salary DESC;
 
 ---------------------------------------------------------------------------------------------------
 --Q3
@@ -101,25 +54,7 @@ ORDER BY "Total Income" DESC;
 
 ---------------------------------------------------------------------------------------------------
 --Q4
-SELECT
-    e.department_id, 
-    d.department_name,
-    e.employee_id, 
-    e.job_id,
-    TO_CHAR(MIN(e.salary), '$999,999.99')AS "Lowest Dept/Job Pay"
-FROM employees e
-JOIN departments d ON e.department_id = d.department_id
-WHERE e.job_id NOT LIKE 'IT%' AND e.job_id NOT LIKE '%REP' AND e.job_id NOT LIKE 'SA%'
-GROUP BY 
-    e.department_id, e.employee_id, e.job_id, d.department_name
-HAVING 
-    MIN(e.salary) BETWEEN 6500 AND 16800
-ORDER BY 
-    e.department_id, 
-    e.job_id;
-    
-    
--- Nicole: Do not need to join.
+
 SELECT
     department_id,
     job_id,
@@ -136,7 +71,7 @@ ORDER BY
 
 ---------------------------------------------------------------------------------------------------
 --Q5
-Select * from employees;
+
 --showing all employees PAID MORE than 10320 (highest pay amongth lowest is Taylor 10320== 8600 + (8600*0.2))
 --USING PAID comparison as per clint's formula salay + salarly*commission_pct
 --Hunold earns 9000, which is less than 10320
@@ -178,6 +113,7 @@ ORDER BY job_id;
 
 
 -- Q6
+--no joins allowed
 SELECT
     last_name,
     salary,
@@ -193,7 +129,8 @@ WHERE (salary*(1+NVL(commission_pct, 0))) > (
 ORDER BY UPPER(last_name);
 
 --Q7
---showing all employees earning more than the BEST PAID unionized worker
+--showing all employees earning less than the BEST PAID unionized worker (value == 14300)
+--unionized worker is NOT in jobID 'AD%' or a manager
 --AND only if they work in sales or marketing, AND NOT PRES or MANAGER
 SELECT
     SUBSTR(UPPER(SUBSTR(first_name,0,1))||  SUBSTR(first_name, 2) || ' ' || UPPER(SUBSTR(last_name,0,1))||  SUBSTR(last_name, 2),0,24) AS full_name,
